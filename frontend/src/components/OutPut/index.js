ed211d97a8b603ddee301ce800c060ef1439435c
SJamal08 <sidikjam08@gmail.com>
2021-10-20 09:37:39 +0200

    setup ci

--- frontend/src/components/OutPut/index.js
+++ frontend/src/components/OutPut/index.js
@@ -1,37 +1,33 @@
 import React from 'react'
 
+
+const Comp = ({msg , color}) => {
+    return <div style={{
+        color 
+    }} className="msg-resolved">
+                {msg}
+            </div>
+}
+const GetCorrectComponent = ({texte}) => {
+    console.log("Le texte est  " , texte)
+
+    if (texte.startsWith("<DESCRIBE::"))
+         return <Comp color={"green"} msg={"Your Ticker is Resolved Successfully"} />
+
+    if (texte.startsWith("<FAILED::"))
+        return <Comp color={"red"} msg={"Your Ticker is pending"} />
+
+    if (texte.startsWith("<ERROR::"))   
+    return <Comp color={"yellow"} msg={"Your Ticker is Closed Successfully"} />
+
+    return <Comp color={"white"} msg={"The Ticket Status is unknown. Check back later."} />
+
+}
+
+
 function OutPut({output}) {
-        if(Array.isArray(output)){
-            output.map((element) => {
-                const tableElement = element.split(">")
-                console.log("case is " , tableElement[0])
-                switch (tableElement[0]) {
-                    case "<DESCRIBE::":
-                         return( 
-                        <div className="msg-resolved">
-                            Your Ticker is Resolved Successfully
-                        </div>
-                    )
-                    case "<FAILED::": return (
-                        <div className="msg-resolved">
-                            Your Ticker is pending
-                        </div>
-                    )
-                    case "<ERROR::": return (
-                        <div className="msg-closed">
-                            Your Ticker is Closed Successfully
-                        </div>
-                    )
-                    default: return(
-                        <div className="msg-empty">
-                            The Ticket Status is unknown. Check back later.
-                        </div>
-                    )
-                }
-            }
-        )} else {
-            return <div style={{color:"white"}}>{output}</div>
-        }
+    if(!Array.isArray(output)) return <div style={{color:"white"}}>{output}</div>
+    return  output.map((element , index) => <GetCorrectComponent key={index} texte={element.split(">")[0]} />)
  }
 
 export default OutPut
